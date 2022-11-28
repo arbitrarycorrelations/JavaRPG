663e959275b34ff3d60fe5f61095d703b806b813
arbitrarycorrelations <25987220+arbitrarycorrelations@users.noreply.github.com>
2022-11-28 12:37:53 -0700

    Fixed accessibility for Entity attributes

--- Entity.java
+++ Entity.java
@@ -2,19 +2,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Entity {
-    private int max_health;
-    private int current_health; 
-    private int max_mp;
-    private int current_mp;
-    private int base_attack;
-    private int total_attack;
-    private int speed;
-    private int defense; 
-    private int resistance; 
-    private Weapon equipped_weapon;
+    protected int max_health;
+    protected int current_health; 
+    protected int max_mp;
+    protected int current_mp;
+    protected int base_attack;
+    protected int total_attack;
+    protected int speed;
+    protected int defense; 
+    protected int resistance; 
+    protected Weapon equipped_weapon;
+    protected Map<Integer, Spell> spells = new HashMap<Integer, Spell>();
 }
 
 class Hero extends Entity {
+
+    Hero(){
+        this.max_health = 100;
+    }
     private Esper equipped_esper;
     private Map<Integer, Spell> spell_points = new HashMap<Integer, Spell>();
 
